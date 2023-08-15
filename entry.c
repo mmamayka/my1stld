0c582ed5a895ba3000e39b9ca5b93cafe97ae17c
mmamayka <mr.green4000@gmail.com>
2023-08-16 00:14:30 +0300

    Add SystemV process context parsing & validation

--- entry.c
+++ entry.c
@@ -20,10 +20,77 @@ typedef struct {
     uint64_t envc;
     char const** envv;
 
+    uint64_t page_size;
+    uint64_t entry_addr;
+    uint64_t phdr_addr;
+    uint64_t phdr_size;
+    uint64_t phdr_count;
+
     pctx_aux_entry_t const* auxv;
 } pctx_data_t;
 
-inline static void pcxt_parse(void const* pctx, pctx_data_t* pctx_data) {
+#define ERR_UNSUPPORTED_FORMAT 1
+#define PCTX_DATA_POISON UINT64_MAX
+
+inline static void pctx_data_init(pctx_data_t* pctx_data) {
+    pctx_data->argv = pctx_data->envv = NULL;
+    pctx_data->auxv = NULL;
+
+    pctx_data->argc = pctx_data->envc = 0;
+
+    pctx_data->page_size = pctx_data->phdr_addr = 
+        pctx_data->phdr_count = pctx_data->phdr_size = PCTX_DATA_POISON;
+}
+
+inline static int pctx_auxv_parse(pctx_data_t* pctx_data) {
+    for(pctx_aux_entry_t const* entry = pctx_data->auxv; 
+            entry->tag != AT_NULL; ++entry) {
+        switch(entry->tag) {
+        case AT_IGNORE:
+            break;
+        case AT_ENTRY:
+            pctx_data->entry_addr = entry->value;
+            break;
+        case AT_EXECFD:
+            return ERR_UNSUPPORTED_FORMAT;
+            break;
+        case AT_PHDR:
+            pctx_data->phdr_addr = entry->value;
+            break;
+        case AT_PHENT:
+            pctx_data->phdr_size = entry->value;
+            break;
+        case AT_PHNUM:
+            pctx_data->phdr_count = entry->value;
+            break;
+        case AT_PAGESZ:
+            pctx_data->page_size = entry->value;
+            break;
+        case AT_BASE:
+            break;
+        case AT_FLAGS:
+            break;
+        case AT_NOTELF:
+            return ERR_UNSUPPORTED_FORMAT;
+            break;
+        case AT_UID:
+            break;
+        case AT_EUID:
+            break;
+        case AT_GID:
+            break;
+        case AT_EGID:
+            break;
+        case AT_PLATFORM:
+            break;
+        }
+    }
+
+    return 0;
+}
+
+inline static void pctx_sysv_header_parse(void const* pctx, 
+        pctx_data_t* pctx_data) {
     pctx_data->argc = *(uint64_t*)pctx;
     pctx_data->argv = (char const**)((uint64_t const*)pctx + 1);
 
@@ -36,6 +103,24 @@ inline static void pcxt_parse(void const* pctx, pctx_data_t* pctx_data) {
 
     pctx_data->auxv = (pctx_aux_entry_t const*)((uint64_t const*)pctx +
         pctx_data->argc + pctx_data->envc + 3);
+}
+
+inline static int pctx_data_validate(pctx_data_t const* pctx_data) {
+    return pctx_data->page_size == PCTX_DATA_POISON ||
+        pctx_data->phdr_addr == PCTX_DATA_POISON ||
+        pctx_data->phdr_count == PCTX_DATA_POISON ||
+        pctx_data->phdr_size == PCTX_DATA_POISON;
+}
+
+inline static int pcxt_parse(void const* pctx, pctx_data_t* pctx_data) {
+    pctx_data_init(pctx_data);
+    pctx_sysv_header_parse(pctx, pctx_data);
+
+    if(pctx_auxv_parse(pctx_data) || pctx_data_validate(pctx_data)) {
+        return ERR_UNSUPPORTED_FORMAT;
+    } else {
+        return 0;
+    }
 }
 
 inline static void pcxt_data_dump(pctx_data_t const* pctx_data) {
@@ -115,9 +200,13 @@ void entry(void* pctx) {
     printf("m1stld started successfully!\n");
 
     pctx_data_t pctx_data;
-    pcxt_parse(pctx, &pctx_data);
-
+    if(!pcxt_parse(pctx, &pctx_data)) {
+        printf("invalid process context descriptor\n");
+        pcxt_data_dump(&pctx_data);
+    }
+#ifndef NDEBUG
     pcxt_data_dump(&pctx_data);
+#endif /* NDEBUG */
 
     for(pctx_aux_entry_t const* entry = pctx_data.auxv; 
             entry->tag != AT_NULL; ++entry) {
